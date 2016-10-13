71b25982d7b18cf700bf0408f04849a9ffd1375a
Robin Otto <tum-sg15-otto-robin@in.tum.de>
2016-10-13 14:12:15 +0200

    camera follows player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smooth = 10.0f;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, new Vector3(target.position.x, target.position.y, -1),
+            Time.deltaTime * smooth);
+    }
+}
