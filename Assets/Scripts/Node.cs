828707bb81319fa90f7ad13023ddd5d048dfd7b8
Schneyza <schneyza.git@web.de>
2016-10-13 13:53:01 +0200

    added node

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Node
+{
+
+    public List<Node> adjecent = new List<Node>();
+    public Node previous = null;
+    public string label = "";
+
+    public void Clear()
+    {
+        previous = null;
+    }
+}
+
