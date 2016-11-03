ba80f51efa02c1f14ba0102c97455c77f0a5bda9
Schneyza <schneyza.git@web.de>
2016-11-03 11:12:42 +0100

    created camera prefab
    Added camera prefab that automatically adjusts itself to different
    screen sizes/devices

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour
 {
 
-    public Transform target;
     public float smooth = 10.0f;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
     void Update()
     {
         if (target != null)
         {
             transform.position = Vector3.Lerp(
-                transform.position, new Vector3(target.position.x, target.position.y, -1),
+                transform.position, new Vector3(target.position.x, target.position.y, transform.position.z),
                 Time.deltaTime * smooth);
         }
     }
